c85c3d8f5375efa0552c5c3d7bb71f7322786c3d
Una Adilovic <adilovicuna@gmail.com>
2021-08-18 00:33:37 +0200

    animation added

--- Game/Scenes/FinalScore.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-onready var score = get_node("../UI/Score")
-
-func display():
-	text = "Final score: %.1f" % score.get("score")
--- Game/Scripts/Score.gd
+++ Game/Scripts/Score.gd
@@ -1,11 +1,15 @@
 extends Label
 
 var score = 0.0
+var game_over = false
 
 func _on_Meter_Passed():
-	score += 0.02
-	text = "Score: %.1f" % score
+	if not game_over:
+		score += 0.02
+		text = "Score: %.1f" % score
 
 func _display_Final_Score():
+	game_over = true
 	text = "Final Score: %.1f" % score
 	set_align(ALIGN_CENTER)
+	rect_position = Vector2(32,100)
