dc313026dfd3564014dce464b9dd3b24c816ed7f
Una Adilović <adilovicuna@gmail.com>
2021-10-14 17:12:13 +0200

    TrapBalls

--- Game/Scripts/Main.gd
+++ Game/Scripts/Main.gd
@@ -7,7 +7,7 @@ onready var score = get_node("UI/Score")
 onready var end = get_node("UI/End")
 
 func _ready():
-	for name in ["TrapI","TrapO","TrapX", "TrapWalls", "TrapHex", "TrapHexO"]:
+	for name in ["TrapI","TrapO","TrapX", "TrapWalls", "TrapHex", "TrapHexO", "TrapBalls"]:
 		tunnels.trap_scenes.append(load("res://Scenes/Trap_scenes/" + name + ".tscn"))
 	tunnels.create_first_level_traps()
 
