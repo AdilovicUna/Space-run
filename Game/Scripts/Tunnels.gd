0e11c2438d7c925e918330681d2aae8ac14f6084
Una Adilović <adilovicuna@gmail.com>
2021-12-26 03:09:17 +0100

    bugs fixed

--- Game/Scripts/Tunnels.gd
+++ Game/Scripts/Tunnels.gd
@@ -7,8 +7,9 @@ onready var hans = get_node("../Hans")
 var trap_scenes = []
 var bug_scenes = []
 var virus_scenes = []
+var bug_translations = [Vector2(-20,0),Vector2(-10,-20),Vector2(10,-20)]
 var rand = RandomNumberGenerator.new()
-var angle = 0
+var angle = 0	
 
 func _physics_process(_delta):
 	#rotates all children of "traps"
@@ -26,7 +27,6 @@ func create_first_level_traps():
 	# pick number of traps to be added
 	var num_of_traps = rand.randi_range(50,65)
 	var x = 1200
-			
 	for n in num_of_traps:
 		# add space between traps
 		x -= rand.randi_range(70,90)
@@ -64,10 +64,31 @@ func create_one_bug(level,x):
 	# pick a trap
 	var i = rand.randi_range(0, len(bug_scenes) - 1)
 	var bug = bug_scenes[i].instance()
+	
+	# pick how many times we rotate the bug
+	# (needed for the tranlsation array, 
+	# otherwise bug wont be properly placed in the tunnel)
+	i = rand.randi_range(0,5)
+	
+	translateBug(bug,i)
 	bug.translation.x = x
 	
 	tunnel.add_child(bug)
+	rotateBug(bug,i)
 
+func translateBug(bug,i):
+	if(i < 3):	
+		bug.translation.y = bug_translations[i].x
+		bug.translation.z = bug_translations[i].y
+	else:	
+		bug.translation.y = bug_translations[i%3].x * (-1)
+		bug.translation.z = bug_translations[i%3].y * (-1)
+		
+func rotateBug(bug,i):
+	# rotate the trap under some angle
+	angle =  i * (PI / 3)	
+	bug.rotate_x(angle)
+	
 func create_one_trap(level,x):
 	# get the level we are making traps for
 	var tunnel = get_child(level)
