dc8ae9c8588751425cd5ca989bb78688234872d8
Una Adilović <adilovicuna@gmail.com>
2022-02-10 00:14:27 +0100

    bugs rotation done

--- Game/Scenes/Other/End.gd
+++ Game/Scenes/Other/End.gd
@@ -3,9 +3,9 @@ extends ColorRect
 onready var main = get_parent().get_parent()
 
 func _unhandled_input(event):
-	# reset the game if enter is pressed
-	if event.is_action_pressed("ui_accept") and $TryAgain.visible:
-		var hans = get_node("../../Hans")
-		hans.free()
-		get_tree().reload_current_scene()
-		get_tree().paused = false
+    # reset the game if enter is pressed
+    if event.is_action_pressed("ui_accept") and $TryAgain.visible:
+        var hans = get_node("../../Hans")
+        hans.free()
+        var _result = get_tree().reload_current_scene()
+        get_tree().paused = false
--- Game/Scripts/Tunnels.gd
+++ Game/Scripts/Tunnels.gd
@@ -6,7 +6,6 @@ onready var hans = get_node("../Hans")
 var trap_scenes = []
 var bug_scenes = []
 var virus_scenes = []
-var bug_virus_translations = [Vector2(-20,0),Vector2(-10,-20),Vector2(10,-20)]
 var rand = RandomNumberGenerator.new()
 var angle = 0	
 
@@ -36,76 +35,36 @@ func create_first_level_traps():
         create_one_obstacle(level, x) 
         
 func create_one_obstacle(level,x):
-    match level:
-        hans.lvl.ONE:
-            create_one_trap(level, x)
-        hans.lvl.TWO:
-            call_create(bug_scenes,level,x)
-        hans.lvl.THREE:
-            call_create(virus_scenes,level,x)
-            
-func call_create(scene,level,x):
-    if rand.randi_range(0,1) == 0:
-        create_one_trap(level,x)  
-    else:
-        create_one_bug_virus(scene,level,x)  
-        
-func delete_obsticle_until_x(level,x):
+    var scene  = pick_scene(level)   
+    # get the level we are making traps for
     var tunnel = get_child(level)
-    for obsticle in tunnel.get_children():
-        if not "light" in obsticle.name and not "torus" in obsticle.name:
-            if obsticle.translation.x > x:
-                obsticle.queue_free()
+    # pick a trap
+    var i = rand.randi_range(0, len(scene) - 1)
+    # make an instance
+    var obstacle = scene[i].instance()
+    obstacle.translation.x = x
+    
+    tunnel.add_child(obstacle)
+    rotate_obstacle(obstacle)
+
+func pick_scene(level,scene = trap_scenes):
+    var r = rand.randi_range(0,1)
+    if level == hans.lvl.TWO and r == 0:
+            scene = bug_scenes
+    elif level == hans.lvl.THREE and r == 0:
+            scene = virus_scenes
+    return scene
+    
+func rotate_obstacle(obstacle):
+    # rotate the obstacle under some angle
+    angle = rand.randi_range(0,5) * (PI / 3)
+    obstacle.rotate_x(angle)
+          
+func delete_obstacle_until_x(level,x):
+    var tunnel = get_child(level)
+    for obstacle in tunnel.get_children():
+        if not "light" in obstacle.name and not "torus" in obstacle.name:
+            if obstacle.translation.x > x:
+                obstacle.queue_free()
             else:
                 return;
-    
-func create_one_trap(level,x):
-    # get the level we are making traps for
-    # pick a trap
-    var tunnel = get_child(level)
-    var i = rand.randi_range(0, len(trap_scenes) - 1)
-    var trap = trap_scenes[i].instance()
-    trap.translation.x = x
-    
-    tunnel.add_child(trap)
-    rotate_Trap(trap)
-
-func rotate_Trap(trap):
-    # rotate the trap under some angle
-    angle = rand.randi_range(0,5) * (PI / 3)
-    trap.rotate_x(angle)
-
-func create_one_bug_virus(scene,level,x):
-    # get the level we are making traps for
-    var tunnel = get_child(level)
-
-    # pick a trap
-    var length = len(bug_scenes) if level == hans.lvl.TWO else len(virus_scenes)
-    var i = rand.randi_range(0, len(scene) - 1)
-    
-    # make an instance
-    var bug_virus = scene[i].instance()
-   
-    # pick how many times we rotate the bug or a virus
-    # (needed for the tranlsation array, 
-    # otherwise it wont be properly placed in the tunnel)
-    i = rand.randi_range(0,5)
-    
-    translate_bug_virus(bug_virus,i)
-    bug_virus.translation.x = x
-    
-    tunnel.add_child(bug_virus)
-    rotate_bug_virus(bug_virus,i)
-  
-func translate_bug_virus(bug_virus,i):
-    if i < 3:	
-        bug_virus.translation.y = bug_virus_translations[i].x
-        bug_virus.translation.z = bug_virus_translations[i].y
-    else:	
-        bug_virus.translation.y = bug_virus_translations[i%3].x * (-1)
-        bug_virus.translation.z = bug_virus_translations[i%3].y * (-1)
-        
-func rotate_bug_virus(bug_virus,i):
-    # rotate bug/virus under some angle
-    angle =  i * (PI / 3)	
-    bug_virus.rotate_x(angle)
