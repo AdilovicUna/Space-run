d87a6fcf8a2548daa19ab33f6d1ace5762c3a904
Una Adilović <adilovicuna@gmail.com>
2022-03-14 17:13:26 +0100

    update

--- Game/Scripts/Other/Score.gd
+++ Game/Scripts/Other/Score.gd
@@ -18,10 +18,3 @@ func _display_Final_Score():
     text = "Final Score: %.1f" % score
     set_align(ALIGN_CENTER)
     rect_position = Vector2(32,120)
-
-func _reset():
-    score = 0.0
-    text = "Score: %.1f" % score
-    game_over = false
-    set_align(ALIGN_LEFT)
-    rect_position = Vector2(32,24)
